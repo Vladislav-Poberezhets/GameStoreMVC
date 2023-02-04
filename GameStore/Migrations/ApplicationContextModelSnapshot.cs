ceb8ac63e1399893e8b3ff6678874cae9ba0b495
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2023-02-04 02:37:24 +0200

    Add Store Change Page, PageOptions

--- GameStore/Migrations/ApplicationContextModelSnapshot.cs
+++ GameStore/Migrations/ApplicationContextModelSnapshot.cs
@@ -16,161 +16,153 @@ namespace GameStore.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.8")
+                .HasAnnotation("ProductVersion", "7.0.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
             modelBuilder.Entity("GameStore.Models.Category", b =>
-            {
-                b.Property<int>("Id")
-                    .ValueGeneratedOnAdd()
-                    .HasColumnType("int");
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
 
-                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                b.Property<string>("Description")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("Description")
+                        .HasColumnType("nvarchar(max)");
 
-                b.Property<string>("Name")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(max)");
 
-                b.HasKey("Id");
+                    b.HasKey("Id");
 
-                b.ToTable("Categories");
-            });
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("GameStore.Models.Game", b =>
+                {
+                    b.Property<int>("GameId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("GameId"));
+
+                    b.Property<int>("CategoryId")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Description")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.HasKey("GameId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("Games");
+                });
 
             modelBuilder.Entity("GameStore.Models.Order", b =>
-            {
-                b.Property<int>("Id")
-                    .ValueGeneratedOnAdd()
-                    .HasColumnType("int");
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
 
-                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                b.Property<string>("Address")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("Address")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
-                b.Property<string>("CustomerName")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("City")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
-                b.Property<bool>("Shipped")
-                    .HasColumnType("bit");
+                    b.Property<string>("CustomerName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
-                b.Property<string>("State")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<bool>("Shipped")
+                        .HasColumnType("bit");
 
-                b.Property<string>("ZipCode")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<int>("ZipCode")
+                        .HasColumnType("int");
 
-                b.HasKey("Id");
+                    b.HasKey("Id");
 
-                b.ToTable("Orders");
-            });
-
-            modelBuilder.Entity("GameStore.Models.OrderLine", b =>
-            {
-                b.Property<int>("Id")
-                    .ValueGeneratedOnAdd()
-                    .HasColumnType("int");
-
-                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                b.Property<int>("OrderId")
-                    .HasColumnType("int");
-
-                b.Property<int>("ProductId")
-                    .HasColumnType("int");
-
-                b.Property<int>("Quantity")
-                    .HasColumnType("int");
-
-                b.HasKey("Id");
-
-                b.HasIndex("OrderId");
-
-                b.HasIndex("ProductId");
-
-                b.ToTable("OrderLines");
-            });
-
-            modelBuilder.Entity("GameStore.Models.Product", b =>
-            {
-                b.Property<int>("Id")
-                    .ValueGeneratedOnAdd()
-                    .HasColumnType("int");
-
-                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                b.Property<int>("CategoryId")
-                    .HasColumnType("int");
-
-                b.Property<string>("Description")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
-
-                b.Property<string>("Name")
-                    .IsRequired()
-                    .HasColumnType("nvarchar(max)");
-
-                b.Property<decimal>("PurchasePrice")
-                    .HasColumnType("decimal(18,2)");
-
-                b.Property<decimal>("RetailPrice")
-                    .HasColumnType("decimal(18,2)");
-
-                b.HasKey("Id");
-
-                b.HasIndex("CategoryId");
-
-                b.ToTable("Products");
-            });
+                    b.ToTable("Orders");
+                });
 
             modelBuilder.Entity("GameStore.Models.OrderLine", b =>
-            {
-                b.HasOne("GameStore.Models.Order", "Order")
-                    .WithMany("Lines")
-                    .HasForeignKey("OrderId")
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .IsRequired();
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
 
-                b.HasOne("GameStore.Models.Product", "Product")
-                    .WithMany()
-                    .HasForeignKey("ProductId")
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .IsRequired();
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                b.Navigation("Order");
+                    b.Property<int>("GameId")
+                        .HasColumnType("int");
 
-                b.Navigation("Product");
-            });
+                    b.Property<int>("OrderId")
+                        .HasColumnType("int");
 
-            modelBuilder.Entity("GameStore.Models.Product", b =>
-            {
-                b.HasOne("GameStore.Models.Category", "Category")
-                    .WithMany("Products")
-                    .HasForeignKey("CategoryId")
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .IsRequired();
+                    b.Property<int>("Quantity")
+                        .HasColumnType("int");
 
-                b.Navigation("Category");
-            });
+                    b.HasKey("Id");
+
+                    b.HasIndex("GameId");
+
+                    b.HasIndex("OrderId");
+
+                    b.ToTable("OrderLines");
+                });
+
+            modelBuilder.Entity("GameStore.Models.Game", b =>
+                {
+                    b.HasOne("GameStore.Models.Category", "Category")
+                        .WithMany("Games")
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Category");
+                });
+
+            modelBuilder.Entity("GameStore.Models.OrderLine", b =>
+                {
+                    b.HasOne("GameStore.Models.Game", "Game")
+                        .WithMany()
+                        .HasForeignKey("GameId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("GameStore.Models.Order", "Order")
+                        .WithMany("Lines")
+                        .HasForeignKey("OrderId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Game");
+
+                    b.Navigation("Order");
+                });
 
             modelBuilder.Entity("GameStore.Models.Category", b =>
-            {
-                b.Navigation("Products");
-            });
+                {
+                    b.Navigation("Games");
+                });
 
             modelBuilder.Entity("GameStore.Models.Order", b =>
-            {
-                b.Navigation("Lines");
-            });
+                {
+                    b.Navigation("Lines");
+                });
 #pragma warning restore 612, 618
         }
     }
